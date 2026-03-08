66a917d680d5541a16d9956ca0cc78fed3d0b9c6
Huang Geyang <Sukebeta@outlook.com>
2026-03-08 12:28:07 +0800

    feat: embed vertical live demo video, add  monthly subscription model and remove unneeded backend files

---
+++ config.local.example.js
@@ -0,0 +1,7 @@
+window.DIGITAL_LIFE_CONFIG = Object.assign(
+  {
+    controlPlaneBaseUrl: 'http://localhost:8787',
+    telegramBotUsername: 'splandour_550w_bot'
+  },
+  window.DIGITAL_LIFE_CONFIG || {}
+);
